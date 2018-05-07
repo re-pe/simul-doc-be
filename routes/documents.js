94b87851f7a32870ecbff59cfb891cb1d437cc52
Aurimas <cincikas.aurimas.jp17@gmail.com>
2018-05-07 15:28:43 +0300

    added get all documents functionality, and pos to documents adds default document to database

--- routes/documents.js
+++ routes/documents.js
@@ -1,9 +1,29 @@
 var express = require('express')
 var router = express.Router()
+var Document = require('../models/document')
 
 /* GET documents listing. */
-router.get('/', function (req, res, next) {
-  res.send('documents')
+router.get('/', function (req, res) {
+  Document.find({}, (err, records) => {
+    if (err) {
+      res.send('error when trying get documents')
+    }
+    res.send(records)
+  })
+})
+
+router.post('/', function (req, res) {
+  const def = new Document()
+  def.owner = 'Default ovner'
+  def.authors.push('Defaul author')
+  def.title = 'Default title'
+  def.content = 'Default content'
+  def.save(error => {
+    if (error) {
+      res.send('Unable to add default document')
+    }
+  })
+  res.send('default user added')
 })
 
 module.exports = router
