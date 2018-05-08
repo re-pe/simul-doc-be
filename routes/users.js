8cdddb3f0c0314a235f3e8e73db8fed1467dc131
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2018-05-08 09:54:25 +0300

    Add user get

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('users');
+  User.find({}, (err, records) => {
+    if (err) {
+      res.send('error when trying get users')
+    };
+    res.send(records);
+  })
 });
+
+router.post('/', function (req, res) {
+  User.create(req.body)
+        .then(usr => {
+          res.send(usr)
+        })
+        .catch(error => {
+          res.send('Unable to add user')
+        })
+})
+
 
 module.exports = router;
