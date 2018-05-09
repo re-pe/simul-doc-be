d7805b17c3763875a561895910202252c0f31430
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2018-05-09 11:37:12 +0300

    var to const

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
-var documentsRouter = require('./routes/documents')
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+const documentsRouter = require('./routes/documents')
 
 // Set up mongoose connection
-var mongoose = require('mongoose')
-var mongoDB = 'mongoDb://localhost/simul'
+const mongoose = require('mongoose')
+const mongoDB = 'mongoDb://localhost/simul'
 mongoose.connect(mongoDB)
 mongoose.Promise = global.Promise
-var db = mongoose.connection
+const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-var app = express()
+const app = express()
 
 // // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-app.use('/documents', documentsRouter)
+app.use('/documents', documentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
