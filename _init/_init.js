30db8baa4cfda74cfc03f09464a4d6bc56911ef4
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2018-05-18 17:01:53 +0300

    Add database filler

--- _init/_init.js
+++ _init/_init.js
@@ -1,85 +1,75 @@
 const User = require("../models/user");
 const Document = require("../models/document");
 
-const userno = 5;
-const docno = 3;
+const USERNO = 5;
+const DOCNO = 5;
 
-const userTempl = {
-  firstName: "Firstname${i}",
-  lastName: "Lastname${i}",
-  email: "email${i}@gmail.com",
-  password: "abcdef${i}"
-};
+const userTemplate = JSON.stringify({
+  firstName: "Firstname${index}",
+  lastName: "Lastname${index}",
+  email: "email${index}@gmail.com",
+  password: "abcdef${index}"
+});
 
-const testUser = {
-  firstName: "Firstname",
-  lastName: "Lastname",
-  email: "email@gmail.com",
-  password: "abcdef0"
-};
+const documentTemplate = JSON.stringify({
+  owner: "${userId}",
+  authors: ["${userId}"],
+  title: "title${index}",
+  content: "content ${index}"
+});
 
-const docTempl = {
-  owner: "${user_id}",
-  authors: ["${user_id}"],
-  title: "title_${i}",
-  content: "content ${i}"
-};
-
-const init = db => {
-  let users = [];
-  db.dropDatabase();
-  const userStr = JSON.stringify(userTempl);
-  const promises = [];
-  for (let i = 0; i < userno; i++) {
-    let newUserStr = userStr.replace(/\$\{i\}/g, i.toString());
-    let user = JSON.parse(newUserStr);
-    console.log("-----------------" + user.toString());
-    promises.push((()=>{
-      return User.create(user)
-        .then(created => {
-          console.log("created => " + created._id);
-          return created._id;
-        })
-        .catch(err => {
-          console.log(err);
-        })
-    })());
+const makeObjectString = (strTempl, variables) => {
+  let result = strTempl;
+  for (let key in variables) {
+    result = result.replace(new RegExp(`\\$\\{${key}\\}`, "g"), variables[key]);
   }
-  console.log('promises', promises);
-  Promise.all(promises).then(values => {
-    console.log("values => " + values);
-  });
-
-  //.then(        // User.create(user, (error, user) => {
-  //   console.log(error);
-  // });
-  // .then(created => {
-  //   console.log(created);
-  //   users.push(created);
-  // })
-  // .catch(err => {
-  //   console.log(err);
-  // });
-  // }
-  //});
-  // return;
-
-  // const docStr = JSON.stringify(docTempl);
-
-  // let docs = [];
-
-  // for (let i = 0; i < docno; i++) {
-  //   let index = i % users.length;
-  //   const user_id = users[index]._id;
-  //   // index = (i + 1) % user_ids.length;
-  //   // const next_id = user_ids[index];
-  //   let newUserStr = userStr.replace(/\$\{user_id\}/g, user_id);
-  //   newUserStr = userStr.replace(/\$\{i\}/g, i.toString());
-  //   let doc = JSON.parse(newDocStr);
-  //   Document.create(docs).then(created => {
-  //     docs.push(created);
-  //   });
-  // }
+  return result;
 };
 
-module.exports = init;
+const makeArrayOfPromises = (model, data) =>
+  data.map(item =>
+    model
+      .create(item)
+      .then(created => {
+        //console.log("created => ", created);
+        return created._id;
+      })
+      .catch(err => {
+        return null;
+      })
+  );
+
+const initDb = db =>
+  Promise.resolve(db.dropDatabase())
+    .then(() =>
+      new Array(USERNO)
+        .fill(null)
+        .map((_, index) => makeObjectString(userTemplate, { index: index }))
+    )
+    .then(userObjStrList => userObjStrList.map(str => JSON.parse(str)))
+    .then(userObjectList => makeArrayOfPromises(User, userObjectList))
+    .then(userPromiseList =>
+      Promise.all(userPromiseList).then(userIdList => userIdList)
+    )
+    .then(userIdList => {
+      console.log("Length of userIdList == " + userIdList.length);
+      return userIdList;
+    })
+    .then(userIdList =>
+      new Array(DOCNO).fill(null).map((_, index) =>
+        makeObjectString(documentTemplate, {
+          index: index,
+          userId: userIdList[index % USERNO]
+        })
+      )
+    )
+    .then(docObjStrList => docObjStrList.map(str => JSON.parse(str)))
+    .then(docObjectList => makeArrayOfPromises(Document, docObjectList))
+    .then(docPromiseList =>
+      Promise.all(docPromiseList).then(docIdList => docIdList)
+    )
+    .then(docIdList =>
+      console.log("Length of docIdList == " + docIdList.length)
+    );
+
+module.exports = initDb;
