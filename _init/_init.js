3f50b7c08b07ea4e3bac73d042feb5b3e9884c31
Redas Peškaitis <redas.peskaitis@gmail.com>
2018-05-25 11:10:42 +0300

    Add async/await to function with promises

--- _init/_init.js
+++ _init/_init.js
@@ -2,7 +2,7 @@ const User = require("../models/user");
 const Document = require("../models/document");
 
 const USERNO = 5;
-const DOCNO = 5;
+const DOCNO = 20;
 
 const userTemplate = JSON.stringify({
   firstName: "Firstname${index}",
@@ -11,7 +11,7 @@ const userTemplate = JSON.stringify({
   password: "abcdef${index}"
 });
 
-const documentTemplate = JSON.stringify({
+const docTemplate = JSON.stringify({
   owner: "${userId}",
   authors: ["${userId}"],
   title: "title${index}",
@@ -31,7 +31,6 @@ const makeArrayOfPromises = (model, data) =>
     model
       .create(item)
       .then(created => {
-        //console.log("created => ", created);
         return created._id;
       })
       .catch(err => {
@@ -39,37 +38,42 @@ const makeArrayOfPromises = (model, data) =>
       })
   );
 
-const initDb = db =>
-  Promise.resolve(db.dropDatabase())
+const initDb = async function(db) {
+  await Promise.resolve(db.dropDatabase())
     .then(() =>
-      new Array(USERNO)
-        .fill(null)
-        .map((_, index) => makeObjectString(userTemplate, { index: index }))
+      new Array(USERNO).fill(null).map((_, index) => {
+        return { index: index };
+      })
+    )
+    .then(userDataList =>
+      userDataList.map(item => makeObjectString(userTemplate, item))
     )
     .then(userObjStrList => userObjStrList.map(str => JSON.parse(str)))
     .then(userObjectList => makeArrayOfPromises(User, userObjectList))
-    .then(userPromiseList =>
-      Promise.all(userPromiseList).then(userIdList => userIdList)
-    )
+    .then(userPromiseList => Promise.all(userPromiseList))
     .then(userIdList => {
-      console.log("Length of userIdList == " + userIdList.length);
+      console.log(
+        "userIdList(" + userIdList.length + ") => [" + userIdList + "]"
+      );
       return userIdList;
     })
     .then(userIdList =>
-      new Array(DOCNO).fill(null).map((_, index) =>
-        makeObjectString(documentTemplate, {
+      new Array(DOCNO).fill(null).map((_, index) => {
+        return {
           index: index,
           userId: userIdList[index % USERNO]
-        })
-      )
+        };
+      })
+    )
+    .then(docDataList =>
+      docDataList.map(item => makeObjectString(docTemplate, item))
     )
     .then(docObjStrList => docObjStrList.map(str => JSON.parse(str)))
     .then(docObjectList => makeArrayOfPromises(Document, docObjectList))
-    .then(docPromiseList =>
-      Promise.all(docPromiseList).then(docIdList => docIdList)
-    )
+    .then(docPromiseList => Promise.all(docPromiseList))
     .then(docIdList =>
-      console.log("Length of docIdList == " + docIdList.length)
+      console.log("docIdList(" + docIdList.length + ") => [" + docIdList + "]")
     );
+};
 
 module.exports = initDb;
