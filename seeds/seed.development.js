352fb30acae03a796c3390c15b946f0a4ea280e6
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2018-05-28 02:58:08 +0300

    Fixes

--- seeds/seed.development.js
+++ seeds/seed.development.js
@@ -1,78 +1,75 @@
 const User = require("../models/user");
 const Document = require("../models/document");
 
-const USERNO = 5;
-const DOCNO = 20;
+const NUMBER_OF_USERS_TO_CREATE = 5;
+const NUMBER_OF_DOCUMENTS_TO_CREATE = 20;
 
-const userTemplate = JSON.stringify({
-  firstName: "Firstname${index}",
-  lastName: "Lastname${index}",
-  email: "email${index}@gmail.com",
-  password: "abcdef${index}"
-});
+const createUserFromTemplate = (index) => JSON.parse(`{
+  "firstName": "Firstname${index}",
+  "lastName": "Lastname${index}",
+  "email": "email${index}@gmail.com",
+  "password": "abcdef${index}"
+}`)
 
-const docTemplate = JSON.stringify({
-  owner: "${userId}",
-  authors: ["${userId}"],
-  title: "title${index}",
-  content: "content ${index}"
-});
+const createDocumentFromTemplate = (userId, index) => JSON.parse(`{
+  "owner": "${userId}",
+  "authors": ["${userId}"],
+  "title": "title${index}",
+  "content": "content ${index}"
+}`)
 
-const makeObjectString = (strTempl, variables) => {
-  let result = strTempl;
-  for (let key in variables) {
-    result = result.replace(new RegExp(`\\$\\{${key}\\}`, "g"), variables[key]);
-  }
-  return result;
-};
+/**
+ * Creates items in database
+ * @param {model} model
+ * @params {Object} data
+ */
 
-const makeArrayOfPromises = (model, data) =>
+const createItemsInDatabase = (model, data) =>
   data.map(item =>
     model
-      .create(item)
-      .then(created => {
-        return created._id;
-      })
-      .catch(err => {
-        return null;
-      })
+    .create(item)
+    .then(created => {
+      return created._id;
+    })
+    .catch(err => {
+      return null;
+    })
   );
 
 async function initDb(db) {
-  let makeUserPromiseList = Promise.resolve(db.dropDatabase())
+  let result;
+  await Promise.resolve(db.dropDatabase())
     .then(() =>
-      new Array(USERNO).fill(null).map((_, index) => {
-        return { index: index };
+      new Array(NUMBER_OF_USERS_TO_CREATE).fill(null).map((_, index) => {
+        return {
+          index
+        };
       })
     )
-    .then(userIndexList =>
-      userIndexList.map(item => makeObjectString(userTemplate, item))
+    .then(userDataList =>
+      userDataList.map(item => createUserFromTemplate(item.index))
     )
-    .then(userObjStrList => userObjStrList.map(str => JSON.parse(str)))
-    .then(userObjectList => makeArrayOfPromises(User, userObjectList));
-
-  const userPromiseList = await makeUserPromiseList;
-  const userIdList = await Promise.all(userPromiseList);
-  console.log(`userIdList(${userIdList.length}) => [${userIdList}]`);
-
-  let makeDocPromiseList = Promise.resolve(userIdList)
+    .then(userObjectList => createItemsInDatabase(User, userObjectList))
+    .then(userPromiseList => Promise.all(userPromiseList))
+    .then(userIdList => {
+      result = { userIdList };
+      return userIdList;
+    })
     .then(userIdList =>
-      new Array(DOCNO).fill(null).map((_, index) => {
+      new Array(NUMBER_OF_DOCUMENTS_TO_CREATE).fill(null).map((_, index) => {
         return {
-          index: index,
-          userId: userIdList[index % USERNO]
+          index,
+          userId: userIdList[index % NUMBER_OF_USERS_TO_CREATE]
         };
       })
     )
     .then(docDataList =>
-      docDataList.map(item => makeObjectString(docTemplate, item))
+      docDataList.map(item => createDocumentFromTemplate(item.userId, item.index))
     )
-    .then(docObjStrList => docObjStrList.map(str => JSON.parse(str)))
-    .then(docObjectList => makeArrayOfPromises(Document, docObjectList));
-
-  let docPromiseList = await makeDocPromiseList;
-  let docIdList = await Promise.all(docPromiseList);
-  console.log(`docIdList(${docIdList.length}) => [${docIdList}]`);
+    .then(docObjectList => createItemsInDatabase(Document, docObjectList))
+    .then(docPromiseList => Promise.all(docPromiseList))
+    .then(docIdList => Object.assign(result, {docIdList}));
+  console.log(result);
 }
 
 module.exports = initDb;
