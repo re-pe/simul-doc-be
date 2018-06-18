646aa965717f243cbdc4b73db4675c1a2b7c45fb
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2018-06-18 03:01:44 +0300

    Rewrite document route with authentication middleware

--- routes/documents.js
+++ routes/documents.js
@@ -12,6 +12,7 @@ const sendError = res =>
     status: 401,
   });
 
+
 const isAuthenticated = (req, res, next) =>
   User.findById(req.signedCookies['simul-doc'])
     .then((user) => {
@@ -28,57 +29,34 @@ router.get('/', isAuthenticated, (req, res, next) =>
     })
     .catch(next));
 
-router.get('/:docId', (req, res, next) =>
-  userLogged(req, res)
-    .then((user) => {
-      if (!user) return null;
-      return Document.findById(req.params.docId)
+
+router.get('/:docId', isAuthenticated, (req, res, next) =>
+  Document.findById(req.params.docId)
+    .populate('owner', '-createdAt -updatedAt')
+    .populate('authors', '-createdAt -updatedAt')
+    .then(document => res.send(document))
+    .catch(next));
+
+router.post('/', isAuthenticated, validator(DocumentBodySchema), (req, res, next) =>
+  Document.create(req.body)
+    .then(createdDocument =>
+      Document.findById(createdDocument.id)
         .populate('owner', '-createdAt -updatedAt')
         .populate('authors', '-createdAt -updatedAt')
-        .then((document) => {
-          res.send(document);
-        })
-        .catch(next);
-    })
+        .then(document => res.send(document))
+        .catch(next))
     .catch(next));
 
-router.post('/', validator(DocumentBodySchema), (req, res, next) =>
-  userLogged(req, res)
-    .then((user) => {
-      if (!user) return null;
-      return Document.create(req.body)
-        .then(createdDocument =>
-          Document.findById(createdDocument.id)
-            .populate('owner', '-createdAt -updatedAt')
-            .populate('authors', '-createdAt -updatedAt')
-            .then(foundDocument => res.send(foundDocument))
-            .catch(next))
-        .catch(next);
-    })
+router.delete('/:docId', isAuthenticated, (req, res, next) =>
+  Document.findByIdAndRemove(req.params.docId)
+    .then(() => res.sendStatus(204))
     .catch(next));
 
-router.delete('/:docId', (req, res, next) =>
-  userLogged(req, res)
-    .then((user) => {
-      if (!user) return null;
-      return Document.findByIdAndRemove(req.params.docId)
-        .then(() => {
-          res.sendStatus(204);
-        })
-        .catch(next);
-    }));
-
-router.put('/:docId', validator(DocumentBodySchema), (req, res, next) =>
-  userLogged(req, res)
-    .then((user) => {
-      if (!user) return null;
-      return Document
-        .findByIdAndUpdate(req.params.docId, req.body, { new: true })
-        .populate('authors', '-createdAt -updatedAt')
-        .then((document) => {
-          res.send(document);
-        })
-        .catch(next);
-    }));
+router.put('/:docId', isAuthenticated, validator(DocumentBodySchema), (req, res, next) =>
+  Document
+    .findByIdAndUpdate(req.params.docId, req.body, { new: true })
+    .populate('authors', '-createdAt -updatedAt')
+    .then(document => res.send(document))
+    .catch(next));
 
 module.exports = router;
