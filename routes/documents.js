b4616ce948d0ec017c9dea4f9571443acd2d2a3d
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2018-06-20 09:02:21 +0300

    Fix to Put document

--- routes/documents.js
+++ routes/documents.js
@@ -8,7 +8,7 @@ const validator = require('express-joi-validator');
 
 
 router.get('/', isAuthenticated, (req, res, next) =>
-  Document.find({ authors: req.user._id })
+  Document.find({ authors: req.user.id })
     .select('-content')
     .then((document) => {
       res.send(document);
@@ -17,7 +17,10 @@ router.get('/', isAuthenticated, (req, res, next) =>
 
 
 router.get('/:docId', isAuthenticated, (req, res, next) =>
-  Document.findById(req.params.docId)
+  Document.findOne({
+    _id: req.params.docId,
+    authors: req.user.id,
+  })
     .populate('owner', '-createdAt -updatedAt')
     .populate('authors', '-createdAt -updatedAt')
     .then(document => res.send(document))
@@ -36,16 +39,17 @@ router.post('/', isAuthenticated, validator(DocumentBodySchema), (req, res, next
 router.delete('/:docId', isAuthenticated, (req, res, next) =>
   Document.findOneAndRemove({
     _id: req.params.docId,
-    owner: req.user._id,
+    owner: req.user.id,
   })
     .then(() => res.sendStatus(204))
     .catch(next));
 
 router.put('/:docId', isAuthenticated, validator(DocumentBodySchema), (req, res, next) =>
   Document
-    .updateOne({
-      _id: req.params.docId, authors: req.user._id,
+    .findOneAndUpdate({
+      _id: req.params.docId, authors: req.user.id,
     }, req.body, { new: true })
+    .populate('owner', '-createdAt -updatedAt')
     .populate('authors', '-createdAt -updatedAt')
     .then(document => res.send(document))
     .catch(next));
